3e29b69d63c7c43d4eeebb606cb8f860685ad2e0
a_lavrishin <snapin2025@gmail.com>
2026-01-25 23:43:34 +0300

    init

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import {Button} from "snapinui"
+
+
+export default function Home() {
+  return (
+    <div >
+      <main >
+            <Button variant={"outlined"}>Test</Button>
+      </main>
+    </div>
+  );
+}
